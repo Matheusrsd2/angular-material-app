a6e93d6808d12e40fc2eb25cfd0c995f7e12dc02
matheusrsd2 <matttheus.reee@gmail.com>
2021-04-18 22:18:51 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import {MaterialModule} from './material/material.module';
+import {EmployeeService} from './services/employee.service'
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employees/employee/employee.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(public service: EmployeeService ) { }
+
+  departments = [
+    { id: 3, value: 'Dep 1' },
+    { id: 2, value: 'Dep 2' },
+    { id: 3, value: 'Dep 3' }
+  ];
+
+  ngOnInit(): void {
+  }
+
+  onClear(){
+    this.service.form.reset();
+  }
+
+}
